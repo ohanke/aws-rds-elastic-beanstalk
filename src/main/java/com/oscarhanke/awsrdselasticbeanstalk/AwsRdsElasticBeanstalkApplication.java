621ed3c1d3dfde4590b15e29ed93900f5c19e659
ohanke <steamoha@wp.pl>
2022-08-24 13:50:03 +0200

    Initial commit

---
+++ src/main/java/com/oscarhanke/awsrdselasticbeanstalk/AwsRdsElasticBeanstalkApplication.java
@@ -0,0 +1,12 @@
+package com.oscarhanke.awsrdselasticbeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsElasticBeanstalkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRdsElasticBeanstalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarhanke/awsrdselasticbeanstalk/AwsRdsElasticBeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.awsrdselasticbeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRdsElasticBeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
